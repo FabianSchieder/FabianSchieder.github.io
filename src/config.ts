7570401b8d3112d009ccf1065157c0fb6772ed48
Fabian Schieder <fabian.schieder@el.htlhl.at>
2024-08-18 22:39:58 +0200

    lkjydhnfgljkhdfg

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config =
-{
-    USER_API_BASE_URL: 'http://localhost:8081',
-    FORUM_API_BASE_URL: 'http://localhost:8080/api/forum',
-    TODO_API_BASE_URL: 'http://localhost:8080/api/todo'
-}
